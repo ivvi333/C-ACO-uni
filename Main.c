fe6cc9514fedca9d5636598167d41ccf257af4fe
shenyanyanya <vanya.toryanik@yandex.ru>
2021-06-25 13:00:20 +0300

    Testing graph input

--- Main.c
+++ Main.c
@@ -2,10 +2,18 @@
 #include "../Graphs/graph.h"
 
 int main(void){
-    int V;
+    int V, E, src, dst, wt;
     printf("Number of vertecies: ");
     scanf("%i", &V);
     struct Graph *G = create_graph(V);
+    printf("Number of edges: ");
+    scanf("%i", &E);
+    for (E; E; E--){
+        printf("Enter the src vertex, dst vertex and edge weight: ");
+        scanf("%i %i %i", &src, &dst, &wt);
+        create_edge(G, src, dst, wt);
+    }
     print_graph(G, V);
+    delete_graph(G, V);
     return 0;
 }
