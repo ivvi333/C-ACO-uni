6c23cd2c3dada39751bfb24b1e6d3e6253c3ef79
shenyanyanya <vanya.toryanik@yandex.ru>
2021-06-25 14:47:33 +0300

    ACO alg parametrs input added

--- Main.c
+++ Main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <windows.h>
 #include "../Graphs/graph.h"
 
 int main(void){
-    int V, E, src, dst, wt;
+    int V, E, src, dst, wt, alpha, beta, Q, tau0;
+    double p;
     printf("Number of vertecies: ");
     scanf("%i", &V);
     struct Graph *G = create_graph(V);
@@ -13,7 +15,16 @@ int main(void){
         scanf("%i %i %i", &src, &dst, &wt);
         create_edge(G, src, dst, wt);
     }
+    printf("\nEntered graph:\n");
     print_graph(G, V);
+    printf("\nEnter ACO algorithm parameters:\n");
+    printf("alpha (pheromone wt) and beta (visibility wt): ");
+    scanf("%i %i", &alpha, &beta);
+    printf("Q (optimal path length): ");
+    scanf("%i", &Q);
+    printf("p (pheromone valitilization coeff.) and tau0 (initial pheromone track): ");
+    scanf("%lf %i", &p, &tau0);
     delete_graph(G, V);
+    system("Pause");
     return 0;
 }
