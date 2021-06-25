14c08a28afa3b1af0d65c21a4468d565583c966e
shenyanyanya <vanya.toryanik@yandex.ru>
2021-06-25 16:37:18 +0300

    ACO_solve added (not final)

--- Main.c
+++ Main.c
@@ -1,10 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
 #include "../Graphs/graph.h"
 
+// Стуктура путь
+struct Path{
+    int V; // количество вершин
+    int l; // общая длина пути
+    int *A; // массив вершин пути
+};
+
+void delete_path(struct Path *P){
+    free(P -> A);
+    free(P);
+}
+
+struct Path *ACO_solve(struct Graph *G, int V, int alpha, int beta, double p, double tau0, int t){
+    int Q = 0;
+    struct Path *P = malloc(sizeof(struct Path));
+    P -> V = V;
+    P -> A = malloc(V * sizeof(int));
+    for (int i = 0; i < V; i++){
+        Q += G -> A[i][(i + 1) % V];
+    }
+    P -> l = Q;
+    double **tau = (double **) malloc(V * sizeof(double *));
+    double **eta = (double **) malloc(V * sizeof(double *));
+    for (int i = 0; i < V; i++){
+        tau[i] = calloc(V, sizeof(double));
+        eta[i] = calloc(V, sizeof(double));
+    }
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++){
+            if (i != j){
+                tau[i][j] = tau0;
+                eta[i][j] = 1/(G -> A[i][j]);
+            }
+        }
+    
+    for (int i = 0; i < V; i++){
+        free(tau[i]);
+        free(eta[i]);
+    }
+    free(tau);
+    free(eta);
+    return P;
+}
+
 int main(void){
-    int V, E, src, dst, wt, alpha, beta, Q, tau0;
-    double p;
+    int V, E, src, dst, wt, alpha, beta, t;
+    double p, tau0;
     printf("Number of vertecies: ");
     scanf("%i", &V);
     struct Graph *G = create_graph(V);
@@ -20,11 +65,13 @@ int main(void){
     printf("\nEnter ACO algorithm parameters:\n");
     printf("alpha (pheromone wt) and beta (visibility wt): ");
     scanf("%i %i", &alpha, &beta);
-    printf("Q (optimal path length): ");
-    scanf("%i", &Q);
     printf("p (pheromone valitilization coeff.) and tau0 (initial pheromone track): ");
-    scanf("%lf %i", &p, &tau0);
+    scanf("%lf %lf", &p, &tau0);
+    printf("Number of iterations: ");
+    scanf("%i", &t);
+    struct Path *P = ACO_solve(G, V, alpha, beta, p, tau0, t);
     delete_graph(G, V);
+    delete_path(P);
     system("Pause");
     return 0;
 }
