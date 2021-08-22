42ec9d7d59cd26c9c551e765255fa312c6ceb1f8
shenyanyanya <vanya.toryanik@yandex.ru>
2021-08-22 16:19:39 +0300

    Stable version

--- Main.c
+++ Main.c
@@ -15,6 +15,13 @@ void delete_path(struct Path *P){
     free(P);
 }
 
+void output_path(struct Path *P, int V){
+    printf("\nPath: %i ", P -> A[0]);
+    for (int i = 0; i < V; i++)
+        printf("--> %i ", P -> A[(i + 1) % V]);
+    printf("\nPath length: %i\n", P -> l);
+}
+
 double probability(int i, int j, int *tabu, int V, int alpha, int beta, double **tau, double **eta){
     if (tabu[j])
         return 0.0;
@@ -27,19 +34,35 @@ double probability(int i, int j, int *tabu, int V, int alpha, int beta, double *
     return P;
 }
 
-struct Path *ACO_solve(struct Graph *G, int V, int alpha, int beta, double p, double tau0, double rf, int t){
+void upd_pheromone(struct Path *P, int V, int Q, double p, double ***tau){
+    double dlt;
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            if (i != j)
+                (*tau)[i][j] = (*tau)[i][j] * (1.0 - p);
+    for (int k = 0; k < V; k++)
+        for (int i = 0; i < V; i++){
+            dlt = (double) Q / (double) P[k].l;
+            (*tau)[P[k].A[i]][P[k].A[(i + 1) % V]] += dlt;
+            (*tau)[P[k].A[(i + 1) % V]][P[k].A[i]] += dlt;
+        }
+}
+
+struct Path ACO_solve(struct Graph *G, int V, int alpha, int beta, double p, double tau0, double rf, int t){
     int Q = 0, i, j, *tabu, rv;
     double sum_p, r;
-    struct Path *BP = malloc(sizeof(struct Path));
-    BP -> A = malloc(V * sizeof(int));
+    struct Path BP;
+    BP.A = malloc(V * sizeof(int));
     for (i = 0; i < V; i++){
-        BP -> A[i] = i;
+        BP.A[i] = i;
         Q += G -> A[i][(i + 1) % V];
     }
-    BP -> l = Q;
-    struct Path *P = malloc(sizeof(struct Path));
-    P -> A = malloc(V * sizeof(int));
-    P -> l = 0;
+    BP.l = Q;
+    struct Path *P = malloc(V * sizeof(struct Path));
+    for (int k = 0; k < V; k++){
+        P[k].A = malloc(V * sizeof(int));
+        P[k].l = 0;
+    }
     double **tau = (double **) malloc(V * sizeof(double *));
     double **eta = (double **) malloc(V * sizeof(double *));
     for (i = 0; i < V; i++){
@@ -50,44 +73,55 @@ struct Path *ACO_solve(struct Graph *G, int V, int alpha, int beta, double p, do
         for (j = 0; j < V; j++){
             if (i != j){
                 tau[i][j] = tau0;
-                eta[i][j] = 1/(float) (G -> A[i][j]);
+                eta[i][j] = 1 / (double) (G -> A[i][j]);
             }
         }
     for (t; t; t--){
         for (int k = 0; k < V; k++){
             i = 0;
             tabu = (int *) calloc(V, sizeof(int));
-            P -> A[i] = k;
+            P[k].A[i] = k;
             tabu[k]++;
             while (i < V){
                 sum_p = 0.0;
                 rv = rand() % V;
-                if (((float) rand() / (float) RAND_MAX < rf) && !tabu[rv]){
-                    P -> l += G -> A[P -> A[i]][rv];
-                    P -> A[++i] = rv;
+                if (((double) rand() / (double) RAND_MAX < rf) && !tabu[rv]){
+                    P[k].l += G -> A[P -> A[i]][rv];
+                    P[k].A[++i] = rv;
                     tabu[rv]++;
                 }
                 else {
                     j = 0;
-                    r = (float) rand() / (float) RAND_MAX;
+                    r = (double) rand() / (double) RAND_MAX;
                     for (j; j < V && sum_p < r; j++)
                         if (!tabu[j])
-                            sum_p += probability(P -> A[i], j, tabu, V, alpha, beta, tau, eta);
-                    j = (j - (sum_p? 1: 0)) % V;
-                    P -> l += G -> A[P -> A[i]][j];
-                    P -> A[++i] = j;
+                            sum_p += probability(P[k].A[i], j, tabu, V, alpha, beta, tau, eta);
+                    if (sum_p)
+                        j = (j - 1) % V;
+                    else
+                        j = k;
+                    P[k].l += G -> A[P[k].A[i]][j];
+                    P[k].A[++i] = j;
                     tabu[j]++;
                 }
             }
+            if (P[k].l < BP.l){
+                BP.l = P[k].l;
+                for (i = 0; i < V; i++)
+                    BP.A[i] = P[k].A[i];
+            }
+            free(tabu);
         }
+        upd_pheromone(P, V, Q, p, &tau);
     }
     for (i = 0; i < V; i++){
         free(tau[i]);
         free(eta[i]);
     }
+    free(P);
     free(tau);
     free(eta);
-    return P;
+    return BP;
 }
 
 int main(void){
@@ -114,9 +148,9 @@ int main(void){
     scanf("%lf", &rf);
     printf("Number of iterations: ");
     scanf("%i", &t);
-    struct Path *P = ACO_solve(G, V, alpha, beta, p, tau0, rf, t);
+    struct Path P = ACO_solve(G, V, alpha, beta, p, tau0, rf, t);
+    output_path(&P, V);
     delete_graph(G, V);
-    delete_path(P);
     system("Pause");
     return 0;
 }
